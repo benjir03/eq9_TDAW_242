356e1ff74c85920695462592dc03a3c6d29ef36e
benjir <joschiki.03crus@gmail.com>
2024-06-02 20:33:05 -0600

    Pantalla home

--- proyectoFinal/php/inicioSesion.php
+++ proyectoFinal/php/inicioSesion.php
@@ -1,7 +1,24 @@
 <?php
+    // Incluir el archivo de conexión
     require "conect.php";
 
+    // Obtener los datos del formulario
     $correo = $_POST["correoSesion"];
     $contrasena = $_POST["contraSesion"];
-    echo "$correo  $contrasena";
+
+    // Realizar la consulta
+    $validar_login = mysqli_query($conn, "SELECT * FROM admin WHERE email = '$correo' AND pass = '$contrasena'");
+
+    // Contar el número de filas
+    $num_rows = mysqli_num_rows($validar_login);
+
+    if ($num_rows > 0) {
+        //echo "Inicio de sesión exitoso. Usuario: $correo";
+        header("location: ../restringido/log_home.php");
+    } else {
+        echo "Correo o contraseña incorrectos.";
+    }
+
+    // Opcional: cerrar la conexión
+    mysqli_close($conn);
 ?>
