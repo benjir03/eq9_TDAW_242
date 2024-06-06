82c94f75c9d9eda61eaba3d30bcdccec5b4a762a
benjir <joschiki.03crus@gmail.com>
2024-06-06 09:11:17 -0600

    Limpiar reg

--- proyectoFinal/views/formularioRegistro.php
+++ proyectoFinal/views/formularioRegistro.php
@@ -140,7 +140,7 @@
         <!-- S C R I P T S -->
         <script>
             function cleanForm() {
-              document.getElementById("registro").reset();
+              document.getElementById("reg").reset();
             }
           </script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
