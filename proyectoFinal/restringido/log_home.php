75a9d77fc0c304c724cf13a35e8e2b407f8ad253
AngelFlores123 <afloresc1804@alumno.ipn.mx>
2024-06-24 12:50:02 -0600

    tabla de datos admin

--- proyectoFinal/restringido/log_home.php
+++ proyectoFinal/restringido/log_home.php
@@ -15,12 +15,12 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <title>ADMIN</title>
+        <title>Admin | Programa de tutorías ESCOM</title>
         <meta name="author" content="Equipo 9">
         
         <!-- S T Y L E S -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link rel="stylesheet" href="../estilos/styles-log_home.css">
+        <link rel="stylesheet" href="../estilos/styles-inicioSesion.css">
         
     </head>
 
@@ -28,11 +28,13 @@
         <!-- HEADER -->
         <header class="p-3 text-bg-dark text-bg-light">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <img src="../imgs/escudoIPN.png" alt="Logo IPN" title="Logo IPN" class="headerImg">
-                <img src="../imgs/escudoESCOM.png" alt="Logo ESCOM" title="Logo ESCOM" class="headerImg">
+                <a href="../index.php" class="nav-brand"><img src="../imgs/escudoIPN.png" alt="Logo IPN" title="Logo IPN" class="headerImg"></a>
+                <a href="../index.php" class="nav-brand"><img src="../imgs/escudoESCOM.png" alt="Logo ESCOM" title="Logo ESCOM" class="headerImg"></a>
 
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="log_home.php" class="nav-link px-2 text-white">Administrador</a></li>
+                    <li><a href="../index.php" class="nav-link px-2 text-secondary">Inicio</a></li>
+                    <li><a href="formularioRegistro.php" class="nav-link px-2 text-secondary">Registro</a></li>
+                    <li><a href="" class="nav-link px-2 text-white">Inicio de sesión</a></li>
                 </ul>
                 <button type="button" class="btn btn-outline-light me-2" onclick="cerrarSesion()">Cerrar sesión</button>
             </div>
@@ -40,9 +42,57 @@
 
         <!-- MAIN -->
         <main>
-            <div class="fondoSesion container-fluid d-flex justify-content-center align-items-center row-full-height">
-                <div class="contenedorFormSesion">
+            <div class="fondoSesion container-fluid d-flex flex-column align-items-center row-full-height">
+                <div class="contenedorFormSesion mb-3">
                     <h1>Hola <?php echo "$username_session"?></h1>
+                </div>
+                <div class="table-responsive w-100">
+                    <table class="table table-striped table-bordered table-hover text-center">
+                        <thead class="table-dark">
+                            <tr>
+                                <th>Boleta</th>
+                                <th>Nombre</th>
+                                <th>Apellido Paterno</th>
+                                <th>Apellido Materno</th>
+                                <th>Teléfono</th>
+                                <th>Semestre</th>
+                                <th>Carrera</th>
+                                <th>Tutoría</th>
+                                <th>Tutor</th>
+                                <th>Correo</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                require "../php/conect.php";
+                                $datos = "SELECT * from alum";
+                                $consulta = mysqli_query($conn,$datos);
+                                while($mostrar = mysqli_fetch_array($consulta)){
+                            ?>
+                            <tr>
+                                <td><?php echo $mostrar['boleta'] ?></td>
+                                <td><?php echo $mostrar['nombre'] ?></td>
+                                <td><?php echo $mostrar['apPat'] ?></td>
+                                <td><?php echo $mostrar['apMat'] ?></td>
+                                <td><?php echo $mostrar['telefono'] ?></td>
+                                <td><?php echo $mostrar['semestre'] ?></td>
+                                <td><?php echo $mostrar['carrera'] ?></td>
+                                <td><?php echo $mostrar['tipoTutoria'] ?></td>
+                                <td><?php echo $mostrar['nombreTutor'] ?></td>
+                                <td><?php echo $mostrar['correo'] ?></td>
+                                <td>
+                                    <div class="btn-group-vertical">
+                                        <button type="button" class="btn btn-danger mb-2">Eliminar</button>
+                                        <button type="button" class="btn btn-warning">Modificar</button>
+                                    </div>
+                                </td>
+                            </tr>
+                            <?php
+                                }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </main>
